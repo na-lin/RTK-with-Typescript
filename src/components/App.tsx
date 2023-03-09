5ed2c868d7609cad40f87a46217a5bdb310cec41
Na Lin <lna483018@gmail.com>
2023-03-08 23:14:40 -0500

    setup project with redux store

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import RepositoriesList from "./RepositoriesList";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h3>Search for Repositories</h3>
+      <RepositoriesList />
+    </div>
+  );
+};
+
+export default App;
