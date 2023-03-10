075c072535a537cb657a0d0f45f21fd5943de58a
Na Lin <lna483018@gmail.com>
2023-03-09 21:25:19 -0500

    create Counter component for RTK reducers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-import RepositoriesList from "./RepositoriesList";
+// import RepositoriesList from "./RepositoriesList";
+import Counter from "./Counter";
 
 const App: React.FC = () => {
   return (
     <div>
-      <h3>Search for Repositories</h3>
-      <RepositoriesList />
+      {/* <h3>Search for Repositories</h3>
+      <RepositoriesList /> */}
+
+      <Counter />
     </div>
   );
 };
---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <h3>Counter</h3>
+      <button>Increase</button>
+      <button>Descrease</button>
+    </div>
+  );
+};
+
+export default Counter;
