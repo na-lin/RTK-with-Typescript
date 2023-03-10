1e0dc70e327322bb258c02bb4590c27ef5642af3
Na Lin <lna483018@gmail.com>
2023-03-09 21:37:41 -0500

    use redux store in Counter component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../state";
+import { increase, descrease } from "../state/slices/counterSlice";
 
 const Counter = () => {
+  // const { count } = useSelector((state) => state.counter);
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
+  const handleIncrease = () => {
+    dispatch(increase());
+  };
+  const handleDecrease = () => {
+    dispatch(descrease());
+  };
+
   return (
     <div>
       <h3>Counter</h3>
-      <button>Increase</button>
-      <button>Descrease</button>
+      <p>{count}</p>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Descrease</button>
     </div>
   );
 };
